1939900dac7165b06e1fa086ed67e02292463e50
Raphael Braun <raphael.braun@zallpy.com>
2021-05-29 08:12:18 -0300

    Removed storyboards

--- TopReddits/TopRedditsViewController.swift
+++ TopReddits/TopRedditsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TopRedditsViewController.swift
 //  TopReddits
 //
 //  Created by Raphael Braun on 29/05/21.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TopRedditsViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    view.backgroundColor = .purple
   }
-
-
 }
-
